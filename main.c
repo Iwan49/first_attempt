019c55679dfa33c95b22369ba323886dd01ca321
pozhiloy_voron <pukanusbomb@gmail.com>
2022-12-04 20:22:37 +0200

    First change

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Hello world!\n"
+           "There is my first work attempt with GitHub\n"
+           "I firmly believe that this technology can support my projects in future.\n"
+           "\n"
+           "\n"
+           "\n"
+           "Created by me(Who cares?)");
+    return 0;
+}
